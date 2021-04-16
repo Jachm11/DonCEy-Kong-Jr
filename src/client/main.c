105063f0e936b15bd3da714c525f0f5dafbd8b14
Jose Alejandro Chavarria <joalchama@gmail.com>
2021-04-15 18:40:40 -0600

    Socket C Java 1 cliente apenas funciona

--- src/client/main.c
+++ src/client/main.c
@@ -1,7 +1,53 @@
 #include <stdio.h>
+#include <conio.h>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib") //Winsock Library
 
+#define PORT 1108
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello,World");
+    WSADATA wsa;
+    SOCKET s;
+    struct sockaddr_in server;
+    char c = 0;
+
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0)
+    {
+        printf("Failed. Error Code : %d.\nPress a key to exit...", WSAGetLastError());
+        c = getch();
+        return 1;
+    }
+
+    printf("Initialised.\n");
+
+    //Create a socket
+    if((s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d.\n", WSAGetLastError());
+        WSACleanup();
+        c = getch();
+        return 1;
+    }
+    printf("Socket created. Connecting...\n");
+    memset(&server, 0, sizeof server);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    //Connect to remote server
+    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        printf("Connect error:%d.\nPress a key to exit...", WSAGetLastError());
+        closesocket(s);
+        WSACleanup();
+        c = getch();
+        return 1;
+    }
+    puts("Connected.\nPress a key to exit...");
+    closesocket(s);
+    WSACleanup();
+    c = getch();
+    return 0;
 }
--- src/server/src/Main.java
+++ src/server/src/Main.java
@@ -1,7 +1,30 @@
-package server.src;
+import java.net.*;
+import java.io.*;
+  
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    private static final int PORT = 1108;
+ 
+    public static void main(String[] args) throws IOException {
+        ServerSocket listener = new ServerSocket(PORT);
+
+        System.out.println("{Server} Waiting for a connection");
+        Socket client = listener.accept();
+        System.out.println("{Server} Connected to client!");
+
+        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        try{
+            while(true){
+                String request = in.readLine();
+                System.out.println("{Server} client says: "+request)
+            }
+        }finally{
+            out.close();
+            in.close();
+        }
+      
     }
 }
