860df663f4729b012be2b4d71235499e4e90af29
natigb <53326851+natigb@users.noreply.github.com>
2021-04-26 01:07:39 -0600

    Update Jugador.java

--- src/server/src/Game/Jugador.java
+++ src/server/src/Game/Jugador.java
@@ -1,5 +1,7 @@
 package Game;
-
+/*
+* Clase para crear un Jugador
+*/
 public class Jugador 
 {
     private Integer vidas;
@@ -7,7 +9,9 @@ public class Jugador
     public Integer liana;
     public Integer x;
     public Integer y;
-
+    /**
+	* Constructor de Jugador
+	*/
     public Jugador()
     {
         liana = 1;
@@ -17,23 +21,38 @@ public class Jugador
         posicion = x + "," + y;
 
     }
+    /**
+	* Función para mover al jugador hacia arriba
+	*/
     public void move_up()
     {
         change_position(x, y-20);
 
     }
-    
+    /**
+	* Funcion para mover al jugador hacia abajo
+	*/
     public void move_down()
     {
         change_position(x, y+20);
 
     }
+    /**
+	* Función para mover el jugador hacia los lados
+	* @param X, posicion en x a la que se quiere mover
+	* @param liana_nueva liana en la que está el jugador despues de moverse
+	*/
     public void move_sideways(Integer X, Integer liana_nueva)
     {
         liana = liana_nueva;
         change_position(X, y);
     }
-    
+    /**
+	* Función para detectar una colision entre el jugador y una objeto
+	* @param player_x posicion en x del objeto
+	* @param player_y posicion en y del objeto
+	* @return true si detecta una colision, false de lo contrario
+	*/
     public Boolean colision(Integer object_x, Integer object_y)
     {
         if((object_x == this.x) && (object_y == this.y))
@@ -46,18 +65,29 @@ public class Jugador
         }
         
     }
+    /**
+    * Función en caso de que haya una colision con un enemigo, devuelv al jugador a la posicion inicial y le resta una vida
+    */
     public void hit()
     {
         change_position(85, 585);
         liana = 1;
         vidas--;
     }
+    /**
+    * Función para devolver al jugador a la posicion inicial
+    */
     public void won()
     {
         change_position(85, 585);
         liana = 1;
 
     }
+    /**
+	* Funcion para cambiar la posicion del jugador
+	* @param X posicion en x
+	* @param Y posicion en Y
+	*/
     private void change_position(Integer X, Integer Y)
     {
         this.x = X;
@@ -65,19 +95,35 @@ public class Jugador
         this.posicion = x + "," + y;
 
     }
+    /**
+    * Función para obtener el valor de la posicion del jugador
+    * @return posicion
+    */
     public String getPosition()
     {
         return this.posicion;
 
     }
+    /**
+    * Función para obtener las viddas del jugador
+    * @return vidas
+    */
     public Integer getVidas()
     {
         return this.vidas;
     }
+    /**
+    * Función para obtener el valor de la posicion de X
+    * @return x
+    */
     public Integer getX()
     {
         return this.x;
     }
+    /**
+    * Función para obtener el valor de la posicion de y
+    * @return y
+    */
     public Integer getY()
     {
         return this.y;
