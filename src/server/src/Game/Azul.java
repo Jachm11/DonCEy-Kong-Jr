30394c66310790b4869467c76b48f78d5971af85
natigb <53326851+natigb@users.noreply.github.com>
2021-04-26 00:39:23 -0600

    Update Azul.java

--- src/server/src/Game/Azul.java
+++ src/server/src/Game/Azul.java
@@ -1,7 +1,15 @@
 package Game;
-
+/**
+Clase hija de cocodrilo para crear enemigos azules
+*/
 public class Azul extends Cocodrilo
 {
+    /**
+    * Constructor de Azul
+    * @param x_position posicion en x
+    * @param y_position posicion en y
+    * @param liana_position datos de la liana
+    */
     public Azul(Integer x_position, Integer y_position, Integer[] liana_position)
     {
         
@@ -11,6 +19,9 @@ public class Azul extends Cocodrilo
         this.posicion = "2," + x_position + "," + y_position;
 
     }
+    /*
+    * Función para mover a un enemigo azul a lo largo de la liana
+    */
     public void move()
     {
         this.y = this.y + velocidad;
