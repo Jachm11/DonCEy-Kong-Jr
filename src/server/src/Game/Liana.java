8cd0f577268d5d77739dfffb0311d10d92d81d4c
natigb <53326851+natigb@users.noreply.github.com>
2021-04-26 01:10:24 -0600

    Update Liana.java

--- src/server/src/Game/Liana.java
+++ src/server/src/Game/Liana.java
@@ -1,12 +1,20 @@
 package Game;
-
+/*
+* Clase para crear una Liana
+*/
 public class Liana 
 {
     private Integer id;
     private Integer x;
     private Integer y_inicio;
     private Integer y_fin;
-
+    /**
+    * Constructor de Liana
+    * @param ID id de la liana
+    * @param X posición en x de la liana
+    * @param y_inicio inicio de la liana en y
+    * @param y_Fin final de la liana en y
+    */
     public Liana(Integer ID, Integer X, Integer y_Inicio, Integer y_Fin )
     {
         
@@ -16,11 +24,18 @@ public class Liana
 
         this.id = ID;
     }
+    /**
+    * Función para obtener el ID de la liana
+    * @return id
+    */
     public Integer getId()
     {
         return id;
     }
-
+    /**
+    * Función para obtener el valor de la posicion de la liana
+    * @return posicion [x, y_inicio, y_fin]
+    */
     public Integer[] getPosicion()
     {
         Integer posicion[] = new Integer[3];
